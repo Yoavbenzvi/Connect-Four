5ac756f2b5b0a771d4e9b948b95f473f591d67d0
YoavBZ <yoav.benzvi@gmail.com>
2020-02-12 13:32:05 +0100

    nm

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Column from './components/Column';
 import './App.css';
 
+// add alternating colors
+//make sure to stop switch if end of row
+//add checkwinner function
+//add display
+
 class App extends React.Component {
 	constructor() {
 		super()
@@ -18,7 +23,12 @@ class App extends React.Component {
 	componentDidMount() {
 		let newBoard = [];
 		for(let x = 0; x < 7; x++) {
-			newBoard.push(<Column key={`column ${x}`} currentPlayer={this.state.currentPlayer} x={x} />)
+			newBoard.push(<Column 
+				key={`column ${x}`} 
+				currentPlayer={this.state.currentPlayer} 
+				changePlayer={this.changePlayer}
+				x={x} 
+			/>)
 		}
 
 		this.setState({
